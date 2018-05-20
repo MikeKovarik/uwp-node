6fe0c365537f237631f9271e0696404226539949
Mike <kenr.mk@gmail.com>
2018-05-21 01:09:43 +0200

    streamlining, RIP systray

--- src/systray.mjs
+++
@@ -1,9 +0,0 @@
-import {EventEmitter} from 'events'
-import {nodeUwpIpc} from './stdio.mjs'
-
-
-export var systray = new EventEmitter
-
-nodeUwpIpc.on('message', e => {
-	console.log('requestreceived', e)
-})
